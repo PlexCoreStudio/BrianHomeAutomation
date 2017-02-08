dadbc415959aa6c1c5fbcf815f0d50fa280d406e
PlexCore Studio <plexcorestudio@gmail.com>
2017-02-08 23:03:33 +0100

    *UPDATES* * Forms Update (More Beutiful) * Input & Output Textbox * Current state of program (Awake or sleeping) * More Commands
    readme.txt file to come, with commands and the project idea!

--- BrianHomeAuto/Form1.Designer.cs
+++ BrianHomeAuto/Form1.Designer.cs
@@ -28,21 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new ns1.BunifuElipse(this.components);
+            this.bunifuCustomLabel1 = new ns1.BunifuCustomLabel();
+            this.stateLable = new ns1.BunifuCustomLabel();
+            this.outputTextbox = new WindowsFormsControlLibrary1.BunifuCustomTextbox();
+            this.inputTextbox = new WindowsFormsControlLibrary1.BunifuCustomTextbox();
             this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(174)))), ((int)(((byte)(96)))));
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(12, 9);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(288, 28);
+            this.bunifuCustomLabel1.TabIndex = 1;
+            this.bunifuCustomLabel1.Text = "Brian Home Automation";
+            // 
+            // stateLable
+            // 
+            this.stateLable.AutoSize = true;
+            this.stateLable.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stateLable.Location = new System.Drawing.Point(12, 72);
+            this.stateLable.Name = "stateLable";
+            this.stateLable.Size = new System.Drawing.Size(109, 19);
+            this.stateLable.TabIndex = 5;
+            this.stateLable.Text = "State: Awake";
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.outputTextbox.BorderColor = System.Drawing.Color.SeaGreen;
+            this.outputTextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.outputTextbox.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextbox.Location = new System.Drawing.Point(12, 121);
+            this.outputTextbox.Multiline = true;
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.Size = new System.Drawing.Size(287, 21);
+            this.outputTextbox.TabIndex = 7;
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.inputTextbox.BorderColor = System.Drawing.Color.SeaGreen;
+            this.inputTextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.inputTextbox.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextbox.Location = new System.Drawing.Point(12, 94);
+            this.inputTextbox.Multiline = true;
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(287, 21);
+            this.inputTextbox.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.ClientSize = new System.Drawing.Size(312, 155);
+            this.Controls.Add(this.inputTextbox);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.stateLable);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BrianHomeAuto";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ns1.BunifuElipse bunifuElipse1;
+        private ns1.BunifuCustomLabel bunifuCustomLabel1;
+        private ns1.BunifuCustomLabel stateLable;
+        private WindowsFormsControlLibrary1.BunifuCustomTextbox inputTextbox;
+        private WindowsFormsControlLibrary1.BunifuCustomTextbox outputTextbox;
     }
 }
 
--- BrianHomeAuto/Form1.cs
+++ BrianHomeAuto/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Synthesis;
 using System.Speech.Recognition;
+using System.Diagnostics;
 
 namespace BrianHomeAuto
 {
@@ -16,11 +17,12 @@ namespace BrianHomeAuto
     {
 
         SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = true;
         Choices list = new Choices();
         public Form1()
         {
             SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
-            list.Add(new String[] { "hello", "how are you", "time" });
+            list.Add(new String[] {"wake", "sleep", "hello", "how are you", "time", "date", "open google", "restart", "update", "open word", "close word"});
 
             Grammar gr = new Grammar(new GrammarBuilder(list));
 
@@ -32,35 +34,84 @@ namespace BrianHomeAuto
                 rec.SetInputToDefaultAudioDevice();
                 rec.RecognizeAsync(RecognizeMode.Multiple);
             }
-            catch { return; }
+            catch
+            {
+                return;
+            }
 
-            s.Speak("Hello, I'm Brian. What can I help you with?");
             InitializeComponent();
+        }
+
+        public void restart()
+        {
+            Application.Restart();
         }
 
         public void say(String h)
         {
             s.Speak(h);
+            outputTextbox.AppendText(h + "\n");
         }
 
         private void rec_SpeachRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             String r = e.Result.Text;
 
-            if(r == "hello")
+            if (r == "wake")
             {
-                say("Hi");
+                stateLable.Text = "State: Awake";
+                say("I'm Awake");
+                wake = true;
+            }
+            if (r == "sleep")
+            {
+                stateLable.Text = "State: Sleep mode";
+                say("I'm sleeping");
+                wake = false;
             }
 
-            if (r == "how are you")
+            if (wake == true)
             {
-                say("I'm good, and you?");
-            }
+                if (r == "restart" || r == "update")
+                {
+                    restart();
+                }
 
-            if (r == "time")
-            {
-                say(DateTime.Now.ToString("h:mm"));
+                if (r == "hello")
+                {
+                    say("Hi");
+                }
+
+                if (r == "how are you")
+                {
+                    say("I'm good, and you?");
+                }
+
+                if (r == "time")
+                {
+                    say(DateTime.Now.ToString("h:mm"));
+                }
+
+                if (r == "date")
+                {
+                    say(DateTime.Now.ToString("M/d/yyyy"));
+                }
+
+                if (r == "open google")
+                {
+                    Process.Start("http://google.com");
+                    say("Opening google");
+                }
+
+                if (r == "open word")
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.FileName = "WINWORD.EXE";
+                    Process.Start(startInfo);
+                    say("opening word");
+                }
             }
+            inputTextbox.AppendText(r + "\n");
         }
 
         private void Form1_Load(object sender, EventArgs e)
